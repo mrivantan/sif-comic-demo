f9d942e8997289417c80222d4fa7bc2abf0b61eb
mrivantan <mr.ivan.tan@gmail.com>
2017-03-18 07:00:22 +0800

    fix bugs

--- script/panel_03.js
+++ script/panel_03.js
@@ -1,18 +1,15 @@
 (function(){
     var bc = [300.595,377.191]; // belly centroid
     var p = [135,395];
-    var r1 = 'rotate(360,'+bc[0]+','+bc[1]+')';
 
-    var col1 = '#212121',
-        col2 = '#eee';
-
-    var dur = 4000;
 
     var svg = d3.select('#svg-03');
-    var bg = svg.selectAll('.p03-bg');
+    var bgEven = svg.selectAll('.p03-bg-even').selectAll('path');
+    var bgOdd = svg.selectAll('.p03-bg-odd').selectAll('path');
     var belly = svg.select('#p03-belly');
-    var hand = svg.select('#p03-hand');
+    var handGroup = svg.select('#p03-hand');
 
+    // SCRIBBLE ARM: creating chain link-----------------
     var nodes = blendNodes(0,600,p[0],p[1],10);
     var links = generateChainLinks(nodes);
     var linkForce = d3.forceLink(links)
@@ -22,74 +19,87 @@
     var simulation = d3.forceSimulation(nodes)
             .force('linkForce', linkForce)
             .velocityDecay(0.1)
-            .on('tick', tick)
-        ;
+            .on('tick', tick);
 
-
-    var curve = hand.insert('path', ':first-child')
+    var curve = handGroup.insert('path', ':first-child')
         .datum(simulation.nodes())
         .style("stroke", '#fff')
         .style("stroke-width", 30)
         .style('fill', 'rgba(0,0,0,0)')
-        .style('stroke-linecap', 'round')
-        ;
+        .style('stroke-linecap', 'round');
 
 
 
-    var handin = hand.select('#hand-in').call(d3.drag()
+    var handGraphic = handGroup.select('#hand-in').call(d3.drag()
             .on("start", dragstarted)
             .on("drag", dragged)
             .on("end", dragended)
     );
 
-    var bgp = bg.selectAll('path').attr('transform', 'scale(1)');
 
-    var pp = nodes[nodes.length-1];
-    var ref = [];
-    var tp = [];    //offset
-    var scaleBool = false;
+    var lastHandNode = nodes[nodes.length-1];
+    var handOriginPoint = {};
+    var nodePositionOffset = [];    //offset
+    lastHandNode.fx = p[0]; lastHandNode.fy = p[1];
+
+    var cole1 = 'rgb(76, 187, 236)',
+        cole2 = 'rgb(210, 10, 17)',
+        colo1 = 'rgb(226, 0, 126)',
+        colo2 = 'rgb(255, 210, 0)';
+    var dur = 2000;
+
+    bgEven.attr('fill', cole1);
+    bgOdd.attr('fill', colo1);
+    cycleColor(bgEven, cole1,cole2);
+    cycleColor(bgOdd, colo1,colo2);
+    function cycleColor(selection, c1, c2){
+        var cFill = selection.attr('fill');
+        selection.transition()
+            .duration(dur)
+            .ease(d3.easeLinear)
+            .attr('fill', function(){
+                return  (cFill == c1) ? c2 : c1 ;
+            } )
+            .on('end', function(){ cycleColor(selection,c1,c2)} );
+    }
+
+
+    
+    // EVENT managers-----------------
     function dragstarted(d) {
         if (!d3.event.active) simulation.alphaTarget(0.3).restart();
 
+        var currentTranslate = getTranslation(handGraphic.attr('transform'));
+        console.log(currentTranslate);
 
-        tp[0] = d3.event.x - p[0];
-        tp[1] = d3.event.y - p[1];
+        nodePositionOffset[0] = d3.event.x - p[0] - currentTranslate[0];
+        nodePositionOffset[1] = d3.event.y - p[1] - currentTranslate[1];
 
-        ref[0] = d3.event.x;
-        ref[1] = d3.event.y;
-
-        svg.select('#p03-bg-07').selectAll('path').call(cycle);
+        handOriginPoint.x = d3.event.x - currentTranslate[0];
+        handOriginPoint.y = d3.event.y - currentTranslate[1];
     }
 
+    var cr = 0;
+    belly.attr('transform', 'rotate('+cr+','+bc[0]+','+bc[1]+')');
     function dragged(d) {
 
-        pp.fx = d3.event.x - tp[0];
-        pp.fy = d3.event.y - tp[1];
+        lastHandNode.fx = d3.event.x - nodePositionOffset[0];
+        lastHandNode.fy = d3.event.y - nodePositionOffset[1];
 
-        handin.attr('transform', 'translate('+
-            (d3.event.x - ref[0]) +','+
-            (d3.event.y - ref[1]) +')');
+        handGraphic.attr('transform', 'translate('+
+            (d3.event.x - handOriginPoint.x) +','+
+            (d3.event.y - handOriginPoint.y) +')');
+
+        belly.attr('transform', 'rotate('+(cr++)+','+bc[0]+','+bc[1]+')');
+        if(cr>360)cr=0;
 
     }
 
     function dragended(d) {
         if (!d3.event.active) simulation.alphaTarget(0);
 
-        bg.selectAll('path').interrupt();
-    }
-
-    var c = 0;
-    function cycle(d) {
-        c++;
-        d3.select(d)
-            .transition().duration(dur).ease(d3.easeQuad)
-            .attrTween("transform", function(){
-                return d3.interpolateString( d3.select(this).attr('transform'), (c%2==0) ? 'scale(1)' : 'scale(1.2)' );
-            } )
-            .on('end', cycle);
 
     }
-
 
 
     function tick(){
@@ -126,6 +136,14 @@
         result[0].fx = x1;
         result[0].fy = y1;
         return result;
+    }
+
+
+    function getTranslation(transform) {
+        var g = document.createElementNS("http://www.w3.org/2000/svg", "g");
+        g.setAttributeNS(null, "transform", transform);
+        var matrix = g.transform.baseVal.consolidate().matrix;
+        return [matrix.e, matrix.f];
     }
 
 })();
